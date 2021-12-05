0802b935159419d15b5e73d043abcdd867785bac
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-05 13:58:58 -0800

    Debugging

--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -3,7 +3,7 @@
 root_folder=$1
 echo $root_folder
 
-sudo /usr/bin/python create_honeyfiles.py --root $root_folder
+sudo /usr/bin/python3 create_honeyfiles.py --root $root_folder
 
 # sudo /sbin/auditctl -w $HOME/.aa.pdf -p war
 
