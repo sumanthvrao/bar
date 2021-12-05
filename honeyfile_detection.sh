23880a82d2dc1d780dd24dfd24f679571a378d26
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-05 13:55:05 -0800

    Checking remote args passed

--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+root_folder=$1
+echo $root_folder
 # sudo /usr/bin/python create_honeyfiles.py
 
 # sudo /sbin/auditctl -w $HOME/.aa.pdf -p war
 
-while inotifywait -e access,attrib,close,open,modify -o ./logs.txt ./root_folder/test1.txt; do 
-	echo test
-done
+# while inotifywait -e access,attrib,close,open,modify -o ./logs.txt ./root_folder/test1.txt; do 
+# 	echo test
+# done
+
+
+
 
