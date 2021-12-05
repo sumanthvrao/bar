807892ab85192b3d4a77d3cd8768f695ce67f5cb
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-05 15:03:26 -0800

    Added static honeyfile tracking

--- create_honeyfiles.py
+++ create_honeyfiles.py
@@ -2,22 +2,17 @@ import os
 import argparse
 
 def main(root_filename):
+    if not root_filename[-1] == '/':
+        root_filename = root_filename + '/'
+    
     with open(os.path.join(root_filename, '.honey1.pdf'), 'wb') as f:
-        f.seek(4095)
-        f.write(b'\0')
-
-    with open(os.path.join(root_filename, '.honey1.jpg'), 'wb') as f:
-        f.seek(4095)
-        f.write(b'\0')
-
-    with open(os.path.join(root_filename, '.honey1.txt'), 'wb') as f:
         f.seek(4095)
         f.write(b'\0')
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--root', dest = 'root', default = './roo/')
+    parser.add_argument('--root', dest = 'root', default = './root_folder/')
     args = parser.parse_args()
 
     main(args.root)
--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -1,15 +1,38 @@
 #!/usr/bin/env bash
 
 root_folder=$1
-echo $root_folder
-
 sudo /usr/bin/python3 create_honeyfiles.py --root $root_folder
 
-# sudo /sbin/auditctl -w $HOME/.aa.pdf -p war
+case $root_folder in
+    */)
+        honey_fname="${root_folder}.honey1.pdf"
+        ;;
+    *)
+        honey_fname="${root_folder}/.honey1.pdf"
+        ;;
+esac
 
-# while inotifywait -e access,attrib,close,open,modify -o ./logs.txt ./root_folder/test1.txt; do 
-# 	echo test
-# done
+for d in $(find $root_folder -type d)
+do
+    if [ "$d" == "$root_folder" ]; then
+        continue
+    fi
+    case $d in 
+    */)
+        sudo ln -s $honey_fname "${d}.honey1.pdf"
+        ;;
+    *)
+        sudo ln -s $honey_fname "${d}/.honey1.pdf"
+        ;;
+    esac 
+done
+
+sudo /sbin/auditctl -w $honey_fname -p war
+
+while inotifywait -q -e access,attrib,close,open,modify $HOME/.aa.pdf; do
+	sudo /sbin/ausearch -f $honey_fname | more | grep -o ' pid=[0-9]* ' | grep -v 'grep' | sed 's/\ pid=//' | tr '\n' ' ' | xargs sudo kill -9 > /dev/null 2>&1
+	echo "CRITICAL: A program tried to access a honey file and was killed."
+done
 
 
 
