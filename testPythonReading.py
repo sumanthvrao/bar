7f6c27459704186da0f14a41e681f7e9e5148a94
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-06 15:10:32 -0800

    Debugging getting the PID of accessed process

--- testPythonReading.py
+++ testPythonReading.py
@@ -1,5 +1,6 @@
 import time
 
-hfile = open('./root_folder/.honey1.pdf', 'rb')
-
+with open('./root_folder/.honey1.pdf', 'rb') as hfile:
+    time.sleep(60)
+print("Out of the loop")
 time.sleep(60)
