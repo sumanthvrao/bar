1763911d4e86f221c44d2ad5c31328cf99466ef3
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-06 14:55:47 -0800

    Debugging getting the PID of accessed process

--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -31,7 +31,7 @@ sudo /sbin/auditctl -w $honey_fname -p war
 
 while inotifywait -q -e access,attrib,close,open,modify $honey_fname; do
 	# pid_this = $(sudo /sbin/ausearch -f $honey_fname | more | grep -o ' pid=[0-9]* ' | grep -v 'grep' | sed 's/\ pid=//' | tr '\n' ' ' | xargs sudo kill -9 > /dev/null 2>&1)
-	pid_this = $(sudo /sbin/ausearch -f $honey_fname | more | grep -o ' pid=[0-9]* ' | grep -v 'grep' | sed 's/\ pid=//' | tr '\n' ' ')
+	pid_this=$(sudo /sbin/ausearch -f $honey_fname | more | grep -o ' pid=[0-9]* ' | grep -v 'grep' | sed 's/\ pid=//' | tr '\n' ' ')
 	echo ${pid_this}
 	echo "CRITICAL: A program tried to access a honey file and was killed."
 done
