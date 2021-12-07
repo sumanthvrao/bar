55788a604350ac40a468c6fa6a5272864fc1e0bd
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-07 08:27:18 -0800

    Debugging Sym Links

--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -17,9 +17,6 @@ case $root_folder in
         ;;
 esac
 
-echo "Root"
-echo "${root_folder}"
-
 case $root_folder in
     */)
         honey_fname="${root_folder}.honey1.pdf"
@@ -28,34 +25,32 @@ case $root_folder in
         honey_fname="${root_folder}/.honey1.pdf"
         ;;
 esac
-echo "HOneyFile"
-echo "${honey_fname}"
 
-# for d in $(find $root_folder -type d)
-# do
-#     if [ "$d" == "$root_folder" ]; then
-#         continue
-#     fi
-#     case $d in 
-#     */)
-#         sudo ln -s $honey_fname "${d}.honey1.pdf"
-#         ;;
-#     *)
-#         sudo ln -s $honey_fname "${d}/.honey1.pdf"
-#         ;;
-#     esac 
-# done
+for d in $(find $root_folder -type d)
+do
+    if [ "$d" == "$root_folder" ]; then
+        continue
+    fi
+    case $d in 
+    */)
+        sudo ln -s $honey_fname "${d}.honey1.pdf"
+        ;;
+    *)
+        sudo ln -s $honey_fname "${d}/.honey1.pdf"
+        ;;
+    esac 
+done
 
-# # sudo /sbin/auditctl -w $honey_fname -p war
+# sudo /sbin/auditctl -w $honey_fname -p war
 
-# while inotifywait -q -e access,attrib,close,open,modify $honey_fname; do
-# 	# pid_this = $(sudo /sbin/ausearch -f $honey_fname | more | grep -o ' pid=[0-9]* ' | grep -v 'grep' | sed 's/\ pid=//' | tr '\n' ' ' | xargs sudo kill -9 > /dev/null 2>&1)
-# 	pid_this=$(lsof | grep $honey_fname)
-#     if [ "$pid_this" ]; then
-# 	    echo ${pid_this}
-# 	    echo "CRITICAL: A program tried to access a honey file and was killed."
-#     fi
-# done
+while inotifywait -q -e access,attrib,close,open,modify $honey_fname; do
+	# pid_this = $(sudo /sbin/ausearch -f $honey_fname | more | grep -o ' pid=[0-9]* ' | grep -v 'grep' | sed 's/\ pid=//' | tr '\n' ' ' | xargs sudo kill -9 > /dev/null 2>&1)
+	pid_this=$(lsof | grep $honey_fname)
+    if [ "$pid_this" ]; then
+	    echo ${pid_this}
+	    echo "CRITICAL: A program tried to access a honey file and was killed."
+    fi
+done
 
 
 
