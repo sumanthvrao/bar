44a7c66e604814cf85649aefea0de73d1e0f68b3
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-07 12:40:41 -0800

    Debugging honeyfile creation for entropy

--- create_honeyfiles.py
+++ create_honeyfiles.py
@@ -5,12 +5,12 @@ def main(root_filename):
     if not root_filename[-1] == '/':
         root_filename = root_filename + '/'
     
-    with open(os.path.join(root_filename, '.honey1.pdf'), 'wb') as f:
+    with open(os.path.join(root_filename, '.honey1.pdf'), 'w') as f:
         for r in range(1000):
                 f.write(str(r))
                 f.write('\n')
         #f.seek(1024)
-        f.write(b'\0')
+        f.write('\0')
 
 if __name__ == '__main__':
 
