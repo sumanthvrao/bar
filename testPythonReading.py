add89a27950c9906b523d486f33aa2a71fc9abed
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-07 07:30:02 -0800

    Debugging

--- testPythonReading.py
+++ testPythonReading.py
@@ -1,6 +1,6 @@
 import time
 
-with open('./root_folder/.honey1.pdf', 'rb') as hfile:
+with open('./root_folder/dir1/.honey1.pdf', 'rb') as hfile:
     time.sleep(60)
 print("Out of the loop")
 time.sleep(60)
