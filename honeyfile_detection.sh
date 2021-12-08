5dde3e0bbae39179771da985c24445d768e6f81a
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-07 17:53:02 -0800

    Debugging for encryption

--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -51,14 +51,13 @@ while inotifywait -q -e access,attrib,open,modify $honey_fname; do
     echo "IN LOOP"
     pid_this=$(sudo ausearch -f $honey_fname | more | grep -o ' pid=[0-9]* ' | grep -v 'grep' | sed 's/\ pid=//' | tail -1 | tr '\n' ' ')
     # pid_this=$(lsof $honey_fname | awk 'NR==2 {print $2}')
-    echo ${pid_this}
     
     if [ "$pid_this" ]; then
         echo ${pid_this}
-        inotifywait -q -e close $honey_fname
+        inotifywait -q -e modify,close $honey_fname
         kill -STOP ${pid_this}
         echo "CRITICAL: A program tried to access a honey file and was suspended. Running checks."
-        file_found=$(find $root_folder | grep $honey_fname)
+        file_found=$(find -name $honey_fname)
         echo ${file_found}
         if [ "$file_found" == "" ]; then
             kill -9 ${pid_this}
