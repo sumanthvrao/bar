c255ad45a233d1472a82a70be9aef406dc887e00
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-07 21:42:51 -0800

    Debugging for encryption

--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -43,6 +43,7 @@ do
     esac 
 done
 
+numSymlinks=$(find -L $root_folder -samefile $honey_fname | wc -l)
 sudo service auditd start
 sudo auditctl -D
 sudo auditctl -w $honey_fname -p war -k user1
@@ -63,6 +64,13 @@ while inotifywait -q -e access,attrib,open,modify $honey_fname; do
             sudo chmod -R 400 $root_folder
             echo "CRITICAL: The program overwrote the honey file and was killed. Permissions of folder set to read only."
             break
+        
+        elif [ $(find -L $root_folder -samefile $honey_fname | wc -l) -eq $numSymlinks ]; then
+            kill -9 ${pid_this}
+            sudo chmod -R 400 $root_folder
+            echo "CRITICAL: The program overwrote a symlink. Permissions of folder set to read only."
+            break
+
         else
             python2 driver.py $honey_fname
             varDriver=$(cat .meanEntropy.txt)
