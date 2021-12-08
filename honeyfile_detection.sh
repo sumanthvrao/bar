0bbf47807305900736edd17554ed41261208f4c6
Abhishek2304 <abhishekgupta2304@gmail.com>
2021-12-07 18:38:54 -0800

    Debugging for encryption

--- honeyfile_detection.sh
+++ honeyfile_detection.sh
@@ -57,9 +57,9 @@ while inotifywait -q -e access,attrib,open,modify $honey_fname; do
         inotifywait -q -e close $honey_fname
         kill -STOP ${pid_this}
         echo "CRITICAL: A program tried to access a honey file and was suspended. Running checks."
-        file_found=$(find -path $honey_fname)
-        echo ${file_found}
-        if [ "$file_found" == "" ]; then
+        # file_found=$(find -path )
+        # echo ${file_found}
+        if [ -f $honey_fname ]; then
             kill -9 ${pid_this}
             sudo chmod -R 400 $root_folder
             echo "CRITICAL: The program overwrote the honey file and was killed. Permissions of folder set to read only."
